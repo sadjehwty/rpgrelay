7cd92001cbc8f42736a6a10b63d2a2cabd309b6a
sadjehwty <sadjehwty@gmail.com>
2012-12-04 20:11:14 +0100

    prosecuzione bin

--- bin/rpgrelay.rb
+++ bin/rpgrelay.rb
@@ -4,6 +4,16 @@ require 'rubygems'
 require 'yaml'
 require 'openpgp'
 require 'openpgp/client/gnupg'
-cfg=YAML.load_file Dir.home+'/.rpgrelay'
+dir=Dir.home+'/.rpgrelay'
+if File.exists? dir
+  cfg=YAML.load_file dir
+elsif system 'gpg -k'
+  ret=`gpg -k`
+  ret.split("\n")[0]
+  cfg={:home=>ret}
+  File.open(dir,'w'){|f| f.write cfg.to_yaml}
+else
+  # chiedo con GUI
+end
 gnu=OpenPGP::Client::GnuPG.new({:homedir=>cfg[:home]})
 gnu.list_keys
